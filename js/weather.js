d627d44790dacbb6cd6887801495e61c3cf90852
Circlewee <pnj1118@naver.com>
2022-02-21 16:04:53 +0900

    임시

--- js/weather.js
+++ js/weather.js
@@ -1,21 +1,21 @@
 const weatherInfo = document.querySelector("#weather span");
 const API_KEY = "97dc4eed0898416e115593d823cf4e97";
 
-function onGeoOk(position) {
-  const lat = position.coords.latitude;
-  const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+// function onGeoOk(position) {
+//   const lat = position.coords.latitude;
+//   const lon = position.coords.longitude;
+//   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      weatherInfo.innerText = `${data.name} / ${data.weather[0].main} ${data.main["temp"]}℃`;
-    });
-}
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       weatherInfo.innerText = `${data.name} / ${data.weather[0].main} ${data.main["temp"]}℃`;
+//     });
+// }
 
-function onGeoError() {
-  alert("Can't fint you. No weather for you.");
-}
+// function onGeoError() {
+//   alert("Can't fint you. No weather for you.");
+// }
 
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
-// weatherInfo.innerText = "Seoul / cloud 3℃";
+// navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+weatherInfo.innerText = "Seoul / cloud 3℃";
