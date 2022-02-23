171bea3fe8b23135413a7440e55747001ae3eb5a
Circlewee <pnj1118@naver.com>
2022-02-23 20:17:47 +0900

    버튼 체크 시 todo 줄긋기 추가

--- js/todo.js
+++ js/todo.js
@@ -6,32 +6,37 @@ function paintToDo(newToDo) {
   const li = document.createElement("li");
   const div = document.createElement("div");
   const todo = document.createElement("span");
-  const btn = document.createElement("button");
+  const deleteButton = document.createElement("button");
+  const checkButton = document.createElement("button");
   const modifyTodo = document.createElement("input");
 
   modifyTodo.classList.add("modifyText", CLASS_HIDDEN);
   li.id = newToDo.id;
   todo.innerText = newToDo.text;
-  btn.innerText = "🍑";
-  btn.classList = "deleteButton";
+  deleteButton.innerText = "🍑";
+  deleteButton.classList.add("deleteButton");
+  checkButton.classList.add("checkButton");
 
   todo.addEventListener("dblclick", updateTodo);
-  btn.addEventListener("click", deleteToDo);
+  deleteButton.addEventListener("click", deleteToDo);
+  checkButton.addEventListener("click", todoClicked);
 
+  div.appendChild(checkButton);
   div.appendChild(todo);
   div.appendChild(modifyTodo);
+  div.appendChild(deleteButton);
   li.appendChild(div);
-  div.appendChild(btn);
   ToDoList.appendChild(li);
 }
 
 function updateTodo(event) {
-  const clickedTodo = event.target;
-  const parentDiv = clickedTodo.parentElement;
-  const modifyText = parentDiv.children[1];
+  console.log(event);
+  const clickedTodo = event.target; //span
+  const parentDiv = clickedTodo.parentElement; //div
+  const modifyText = parentDiv.children[2]; // modifyText
 
   modifyText.value = clickedTodo.innerText;
-  parentDiv.children[2].classList.add(CLASS_HIDDEN);
+  parentDiv.children[3].classList.add(CLASS_HIDDEN);
   clickedTodo.classList.add(CLASS_HIDDEN);
   modifyText.classList.remove(CLASS_HIDDEN);
 
@@ -46,7 +51,7 @@ function updateTodo(event) {
       });
       modifyText.classList.add(CLASS_HIDDEN);
       savingToDos();
-      parentDiv.children[2].classList.remove(CLASS_HIDDEN);
+      parentDiv.children[3].classList.remove(CLASS_HIDDEN);
       clickedTodo.classList.remove(CLASS_HIDDEN);
     }
   });
@@ -66,10 +71,13 @@ function handleToDoSubmit(event) {
   savingToDos();
 }
 
-// function todoClicked(event) {
-//   const li = event.target;
-//   li.classList.toggle("textcenterline");
-// }
+function todoClicked(event) {
+  const todoDiv = event.target.parentElement;
+  const todoSpan = todoDiv.children[1];
+
+  todoSpan.classList.toggle("textcenterline");
+  event.target.classList.toggle("indianred");
+}
 
 function deleteToDo(event) {
   const deleteTodo = event.target.parentElement.parentElement; //li
