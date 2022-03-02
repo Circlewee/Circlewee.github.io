731bd9dafc4926103120da5fe864bc01328681b1
Circlewee <pnj1118@naver.com>
2022-03-02 17:32:48 +0900

    검색 기능 추가

---
+++ js/selectform.js
@@ -0,0 +1,17 @@
+function handleSearch(event) {
+  event.preventDefault();
+
+  const engine = event.target[0].value;
+  let value = event.target[1].value;
+
+  if (engine === "Naver") {
+    location = "https://search.naver.com/search.naver?query=" + value;
+  } else if (engine === "Google") {
+    location = "https://www.google.co.kr/search?q=" + value;
+  }
+
+  selectform.querySelector("input").value = "";
+}
+
+const selectform = document.querySelector(".selectform");
+selectform.addEventListener("submit", handleSearch);
