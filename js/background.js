0336f16b33f094a364482efd0eb81e5a3c169602
Circlewee <pnj1118@naver.com>
2022-03-07 20:21:33 +0900

    디자인 변경

--- js/background.js
+++ js/background.js
@@ -6,6 +6,7 @@ function settingClick() {
     applyButton.classList.add("applybutton");
     applyButton.innerText = "Apply";
     applyButton.addEventListener("click", backgroundColorChange);
+    buttonColorChange(applyButton, savedColor);
     clrPicker.appendChild(applyButton);
   }
 
@@ -23,6 +24,7 @@ function settingClick() {
 function backgroundColorChange(event) {
   const clrPicker = event.path[1];
   const clrValue = clrPicker.children[0].value;
+  const applyButton = document.querySelector(".applybutton");
 
   if (clrValue !== background.style.backgroundColor) {
     background.style.backgroundColor = clrValue;
@@ -32,10 +34,15 @@ function backgroundColorChange(event) {
   }
   colorPalette.classList.add("hidden");
   clrPicker.classList.remove("clr-open");
+  buttonColorChange(applyButton, savedColor);
 }
 
 function savingColor(color) {
   localStorage.setItem(COLOR_KEY, color);
+}
+
+function buttonColorChange(button, color) {
+  button.style.borderColor = color;
 }
 
 const background = document.body;
