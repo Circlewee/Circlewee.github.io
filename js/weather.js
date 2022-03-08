9769b6f20a2b32064c3c0dda78bc8f503a318877
Circlewee <pnj1118@naver.com>
2022-03-09 02:58:09 +0900

    1차 날씨 업데이트

--- js/weather.js
+++ js/weather.js
@@ -1,21 +1,73 @@
-const weatherInfo = document.querySelector("#weather span");
+const weather = document.querySelector("#weather");
 const API_KEY = "97dc4eed0898416e115593d823cf4e97";
 
 function onGeoOk(position) {
+  const nowTime = document.createElement("div");
+  const loadingIcon = document.querySelector(".loadingGif");
+
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=${API_KEY}&units=metric&lang=kr`;
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      weatherInfo.innerText = `${data.name} / ${data.weather[0].main} ${data.main["temp"]}℃`;
-    });
+  nowTime.classList.add("time");
+
+  async function getData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data; // response.json();
+  }
+
+  getData(url).then((data) => {
+    const Now = new Date(data.current.dt * 1000);
+    const Tomorrow = new Date(data.daily[1].dt * 1000);
+    const Tomorrow_ = new Date(data.daily[2].dt * 1000);
+    nowTime.innerText = `${makePadStyle(Now.getHours())}:${makePadStyle(Now.getMinutes())}`;
+
+    loadingIcon.remove();
+    weather.appendChild(nowTime);
+    a(data.daily[0], Now);
+    a(data.daily[1], Tomorrow);
+    a(data.daily[2], Tomorrow_);
+  });
+
+  function a(data, date) {
+    const weatherInfoBox = document.createElement("div");
+    const weatherCel = document.createElement("span");
+    const weatherIcon = new Image();
+    const dateSpan = document.createElement("span");
+
+    dateSpan.innerText = `${date.getMonth() + 1}.${date.getDate()}`;
+    weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    weatherInfoBox.classList.add("weatherInfoBox");
+    weatherCel.innerText = `${data.temp.day}℃`;
+    weatherInfoBox.prepend(weatherIcon);
+    weatherInfoBox.append(weatherCel);
+    weatherInfoBox.append(dateSpan);
+    weather.appendChild(weatherInfoBox);
+  }
 }
 
 function onGeoError() {
   alert("Can't fint you. No weather for you.");
 }
 
+function makePadStyle(input) {
+  return String(input).padStart(2, "0");
+}
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
-// weatherInfo.innerText = "Seoul / cloud 3℃";
+
+// const weatherInfo = document.createElement("span");
+// const icon = new Image();
+// icon.src = `http://openweathermap.org/img/wn/04d@2x.png`;
+// weatherInfo.innerText = `Seoul / cloud 3℃`;
+// weatherInfo.append(icon);
+// weather.appendChild(weatherInfo);
+
+// fetch(url)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const date = new Date(data.current.dt * 1000);
+//     weatherIcon.src = `http://openweathermap.org/img/wn/04d@2x.png`;
+
+//     weatherInfo.innerText = `${makePadStyle(date.getHours())}:${makePadStyle(date.getMinutes())} / ${data.current.temp}℃`;
+//   });
