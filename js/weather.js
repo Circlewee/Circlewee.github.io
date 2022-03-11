22601b45fa2e728ce2009daf2c1b3386fe5139ed
Circlewee <pnj1118@naver.com>
2022-03-11 21:31:53 +0900

    날씨 기능 대수정

--- js/weather.js
+++ js/weather.js
@@ -1,15 +1,13 @@
-const weather = document.querySelector("#weather");
-const API_KEY = "97dc4eed0898416e115593d823cf4e97";
-
 function onGeoOk(position) {
-  const nowTime = document.createElement("div");
+  const nowTime = document.querySelector(".time");
   const loadingIcon = document.querySelector(".loadingGif");
+  let Now;
 
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=${API_KEY}&units=metric&lang=kr`;
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${API_KEY}&units=metric&lang=kr`;
 
-  nowTime.classList.add("time");
+  nowTime.children[0].addEventListener("click", refreshClick);
 
   async function getData(url) {
     const response = await fetch(url);
@@ -18,58 +16,56 @@ function onGeoOk(position) {
   }
 
   getData(url).then((data) => {
-    const Now = new Date(data.current.dt * 1000);
-    const Tomorrow = new Date(data.daily[1].dt * 1000);
-    const Tomorrow_ = new Date(data.daily[2].dt * 1000);
-    nowTime.innerText = `${makePadStyle(Now.getHours())}:${makePadStyle(Now.getMinutes())}`;
+    Now = new Date(data.current.dt * 1000);
+
+    nowTime.children[1].innerText = `${makePadStyle(Now.getHours())}:${makePadStyle(Now.getMinutes())}`;
 
     loadingIcon.remove();
-    weather.appendChild(nowTime);
-    a(data.daily[0], Now);
-    a(data.daily[1], Tomorrow);
-    a(data.daily[2], Tomorrow_);
+    nowTime.classList.remove("hidden");
+    paintWeatherInfo(data.current);
+    paintWeatherInfo(data.daily[1]);
+    paintWeatherInfo(data.daily[2]);
   });
 
-  function a(data, date) {
+  function paintWeatherInfo(data) {
     const weatherInfoBox = document.createElement("div");
     const weatherCel = document.createElement("span");
     const weatherIcon = new Image();
     const dateSpan = document.createElement("span");
+    const date = new Date(data.dt * 1000);
 
-    dateSpan.innerText = `${date.getMonth() + 1}.${date.getDate()}`;
+    if (date.getMonth() === Now.getMonth() && date.getDate() === Now.getDate()) {
+      dateSpan.innerText = `Current`;
+      weatherCel.innerText = `${String(data.temp).split(".")[0]}º`;
+    } else {
+      dateSpan.innerText = `${date.getMonth() + 1}.${date.getDate()}`;
+      weatherCel.innerText = `${String(data.temp.min).split(".")[0]}º / ${String(data.temp.max).split(".")[0]}º`;
+    }
+
     weatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
     weatherInfoBox.classList.add("weatherInfoBox");
-    weatherCel.innerText = `${data.temp.day}℃`;
     weatherInfoBox.prepend(weatherIcon);
     weatherInfoBox.append(weatherCel);
-    weatherInfoBox.append(dateSpan);
+    weatherInfoBox.prepend(dateSpan);
     weather.appendChild(weatherInfoBox);
+  }
+
+  function refreshClick() {
+    // write refresh function...
+  }
+
+  function makePadStyle(input) {
+    return String(input).padStart(2, "0");
   }
 }
 
 function onGeoError() {
-  alert("Can't fint you. No weather for you.");
   const loadingIcon = document.querySelector(".loadingGif");
   loadingIcon.remove();
+  alert("Can't fint you. No weather for you.");
 }
 
-function makePadStyle(input) {
-  return String(input).padStart(2, "0");
-}
+const weather = document.querySelector("#weather");
+const API_KEY = "97dc4eed0898416e115593d823cf4e97";
+
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
-
-// const weatherInfo = document.createElement("span");
-// const icon = new Image();
-// icon.src = `http://openweathermap.org/img/wn/04d@2x.png`;
-// weatherInfo.innerText = `Seoul / cloud 3℃`;
-// weatherInfo.append(icon);
-// weather.appendChild(weatherInfo);
-
-// fetch(url)
-//   .then((response) => response.json())
-//   .then((data) => {
-//     const date = new Date(data.current.dt * 1000);
-//     weatherIcon.src = `http://openweathermap.org/img/wn/04d@2x.png`;
-
-//     weatherInfo.innerText = `${makePadStyle(date.getHours())}:${makePadStyle(date.getMinutes())} / ${data.current.temp}℃`;
-//   });
